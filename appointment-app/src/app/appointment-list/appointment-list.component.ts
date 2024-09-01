0cbc877dd18e56ad55af78c08cca29e6bfad1efc
lcaohoanq <hoangclw@gmail.com>
2024-09-01 10:13:20 +0700

    feat: add one way databinding

--- appointment-app/src/app/appointment-list/appointment-list.component.ts
+++ appointment-app/src/app/appointment-list/appointment-list.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
+ 
+  appointment: string = "Hahahahahhahah"
 
 }
