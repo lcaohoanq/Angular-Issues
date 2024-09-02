befbf38363961ac6c67f8fd1bd9d31753406fbc2
lcaohoanq <hoangclw@gmail.com>
2024-09-02 16:21:32 +0700

    feat: implement update reservation

--- hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -1,23 +1,24 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ReservationService } from '../reservation/reservation.service';
 import { Reservation } from '../models/reservation';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent implements OnInit{
+export class ReservationFormComponent implements OnInit {
 
   reservationForm: FormGroup = new FormGroup({});
 
   constructor(
-    private formBuilder: FormBuilder, 
+    private formBuilder: FormBuilder,
     private reservationService: ReservationService,
-    private router: Router) {
-    
+    private router: Router,
+    private activatedRoute: ActivatedRoute) {
+
   }
 
   ngOnInit() {
@@ -29,14 +30,37 @@ export class ReservationFormComponent implements OnInit{
       guestEmail: ['', [Validators.required, Validators.email]],
       roomNumber: ['', Validators.required]
     });
+
+    //check the current url id to find existed reservation
+    let id = this.activatedRoute.snapshot.paramMap.get('id')
+    if (id) {
+      //load the data
+      let reservation = this.reservationService.getReservation(id)
+      if (reservation) {
+        //pathValue mean that you path all the value 
+        //from this reservation to the reservationForm
+        this.reservationForm.patchValue(reservation)
+
+      }
+    }
+
   }
 
-  onSubmit(){
+  onSubmit() {
     //if the above form is valid
-    if(this.reservationForm.valid){
+    if (this.reservationForm.valid) {
       let reservation: Reservation = this.reservationForm.value
-      this.reservationService.addReservation(reservation)
-      
+
+      //we need to identify which action is adding, updating
+      let id = this.activatedRoute.snapshot.paramMap.get('id')
+      if (id) {
+        //update
+        this.reservationService.updateReservation(id, reservation)
+      } else {
+        //add
+        this.reservationService.addReservation(reservation)
+      }
+
       this.router.navigate(['/list'])
     }
   }
